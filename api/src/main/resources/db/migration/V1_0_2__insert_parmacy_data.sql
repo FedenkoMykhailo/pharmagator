78a297e4d95a53d2ede4c0298eda7f10828c5bdf
FedenkoM <fedenkomisha@gmail.com>
2021-10-21 00:03:11 +0300

    added db migration for inserting pharmacy data

---
+++ api/src/main/resources/db/migration/V1_0_2__insert_parmacy_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO pharmacies(id, name, medicine_link_template)
+VALUES (0, 'Dummy', 'http://dummy/{externalId}'),
+       (1, 'Apteka-DS', 'https://some-template/{externalId}'),
+       (2, 'Liki24', 'https://liki24.com/uk/p/{externalId}');
