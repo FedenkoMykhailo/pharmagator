945f5512795174b2df8328e74de149674a35c976
FedenkoM <fedenkomisha@gmail.com>
2021-10-21 01:00:39 +0300

    implement MedicineController tests

---
+++ api/src/test/java/com/eleks/academy/pharmagator/impl/MedicineServiceImplTest.java
@@ -0,0 +1,31 @@
+package com.eleks.academy.pharmagator.impl;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@Disabled("Not implemented yet")
+class MedicineServiceImplTest {
+
+    @Test
+    void testFindAllMedicine() {
+    }
+
+    @Test
+    void testFindMedicineById() {
+    }
+
+    @Test
+    void testSaveMedicine() {
+    }
+
+    @Test
+    void testUpdateMedicine() {
+    }
+
+    @Test
+    void testDeleteMedicine() {
+    }
+}
