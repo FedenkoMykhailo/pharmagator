e839a90123e409cc681c0ac17d17be2c681025be
FedenkoM <fedenkomisha@gmail.com>
2021-10-21 00:05:10 +0300

    added swagger configurations

---
+++ api/src/main/java/com/eleks/academy/pharmagator/constants/SwaggerConstants.java
@@ -0,0 +1,15 @@
+package com.eleks.academy.pharmagator.constants;
+
+public class SwaggerConstants {
+    public static final String API_TITLE = "Swagger API To Pharmagator System";
+    public static final String API_DESCRIPTION = "Api reference Example By JAVA-CAMP";
+    public static final String API_VERSION = "1.0.0";
+
+    public static final String TERM_OF_SERVICE = "Public API";
+    public static final String LICENSE = "API License Open";
+    public static final String LICENSE_URL = "API License Open";
+    public static final String CONTACT_NAME = "JAVA-CAMP";
+    public static final String CONTACT_URL = "https://github.com/FedenkoMykhailo/pharmagator";
+    public static final String CONTACT_EMAIL = "https://github.com/FedenkoMykhailo/pharmagator";
+
+}
